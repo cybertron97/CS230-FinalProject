0669d1ca0dae7433d43a385b1aed115dcd148b8f
Ethan Valdez <ethan@valdezonline.com>
2020-03-23 12:50:02 -0700

    Final update to project files

--- MyGame.py
+++ MyGame.py
@@ -1,126 +1,227 @@
+#imports
 import arcade
+import random
 import math 
+import os 
+import timeit
+import threading
+import time
 
+from tkinter import *
+from tkinter.ttk import *
+import tkinter as tk
+
+# Set up the constants
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Shape"
+SCREEN_GAME_Height = 300
+SCREEN_GAME_Width = 400
+SCREEN_TITLE = "CS 230 Final Project!"
 
+SQUARE_LENGTH = 20
 
-def make_person(head_radius,
-                chest_height,
-                chest_width,
-                leg_width,
-                leg_height,
-                arm_width,
-                arm_length,
-                arm_gap,
-                shoulder_height):
+NUMBER_OF_SHAPES = 1
+lastChoice = "Right"
 
-    shape_list = arcade.ShapeElementList()
+class Shape:
+    def __init__(self, x, y, width, height, angle, delta_x, delta_y,
+                 delta_angle, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.angle = angle
+        self.delta_x = delta_x
+        self.delta_y = delta_y
+        self.delta_angle = delta_angle
+        self.color = color
 
-    # Head
-    shape = arcade.create_ellipse_filled(0, chest_height / 2 + head_radius, head_radius, head_radius,
-                                         arcade.color.WHITE)
-    shape_list.append(shape)
+    def move(self):
+        self.x += self.delta_x
+        self.y += self.delta_y
+        self.angle += self.delta_angle
 
-    # Chest
-    shape = arcade.create_rectangle_filled(0, 0, chest_width, chest_height, arcade.color.BLACK)
-    shape_list.append(shape)
+class Ellipse(Shape):
+    def draw(self):
+        arcade.draw_ellipse_filled(self.x, self.y, self.width, self.height,
+                                   self.color, self.angle)
 
-    # Left leg
-    shape = arcade.create_rectangle_filled(-(chest_width / 2) + leg_width / 2, -(chest_height / 2) - leg_height / 2,
-                                           leg_width, leg_height, arcade.color.RED)
-    shape_list.append(shape)
+class Rectangle(Shape):
+    def draw(self):
+        arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height,
+                                     self.color, self.angle)
 
-    # Right leg
-    shape = arcade.create_rectangle_filled((chest_width / 2) - leg_width / 2, -(chest_height / 2) - leg_height / 2,
-                                           leg_width, leg_height, arcade.color.RED)
-    shape_list.append(shape)
-
-    # Left arm
-    shape = arcade.create_rectangle_filled(-(chest_width / 2) - arm_width / 2 - arm_gap,
-                                           (chest_height / 2) - arm_length / 2 - shoulder_height, arm_width, arm_length,
-                                           arcade.color.BLUE)
-    shape_list.append(shape)
-
-    # Left shoulder
-    shape = arcade.create_rectangle_filled(-(chest_width / 2) - (arm_gap + arm_width) / 2,
-                                           (chest_height / 2) - shoulder_height / 2, arm_gap + arm_width,
-                                           shoulder_height, arcade.color.BLUE_BELL)
-    shape_list.append(shape)
-
-    # Right arm
-    shape = arcade.create_rectangle_filled((chest_width / 2) + arm_width / 2 + arm_gap,
-                                           (chest_height / 2) - arm_length / 2 - shoulder_height, arm_width, arm_length,
-                                           arcade.color.BLUE)
-    shape_list.append(shape)
-
-    # Right shoulder
-    shape = arcade.create_rectangle_filled((chest_width / 2) + (arm_gap + arm_width) / 2,
-                                           (chest_height / 2) - shoulder_height / 2, arm_gap + arm_width,
-                                           shoulder_height, arcade.color.BLUE_BELL)
-    shape_list.append(shape)
-
-    return shape_list
-
-
+class RectangleOutline(Shape):
+    def draw(self):
+        arcade.draw_rectangle_outline(self.x, self.y, self.width, self.height,
+                                     self.color, 3)
+class Line(Shape):
+    def draw(self):
+        newX = 25 * math.cos(self.angle * math.pi / 180)
+        newY = 25 * math.sin(self.angle * math.pi / 180)
+        arcade.draw_line(self.x, self.y, self.x + newX, self.y + newY, self.color, 3)
+        arcade.draw_ellipse_filled(self.x + newX, self.y + newY, 10, 10, self.color, 3)
+        
 class MyGame(arcade.Window):
     """ Main application class. """
 
     def __init__(self):
-        """ Initializer """
-        # Call the parent class initializer
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-
-        head_radius = 30
-        chest_height = 110
-        chest_width = 70
-        leg_width = 20
-        leg_height = 80
-        arm_width = 15
-        arm_length = 70
-        arm_gap = 10
-        shoulder_height = 15
-
-        self.shape_list = make_person(head_radius,
-                                      chest_height,
-                                      chest_width,
-                                      leg_width,
-                                      leg_height,
-                                      arm_width,
-                                      arm_length,
-                                      arm_gap,
-                                      shoulder_height)
-
+        file_path = os.path.join(os.path.dirname(__file__))
+        os.chdir(file_path)
+        self.shape_list = None
         arcade.set_background_color(arcade.color.BOLE)
+        self.size = 100
+        self.isRunning = False
 
     def setup(self):
-
         """ Set up the game and initialize the variables. """
+        self.shape_list = []
 
+        x = random.randrange(SQUARE_LENGTH, SCREEN_WIDTH - SQUARE_LENGTH)
+        y = SQUARE_LENGTH / 2
+
+        base = Rectangle(x, y, SQUARE_LENGTH, SQUARE_LENGTH, 0, 0, 0, 0, arcade.color.YELLOW)
+        self.shape_list.append(base)
+        
+        line = Line(x, y, 0, 0, 0, 0, 0, 0, arcade.color.BLACK)
+        self.shape_list.append(line)
+
+        if(x > SCREEN_WIDTH - x):
+            self.targetX = random.randrange(int(self.size / 2), int(x - SQUARE_LENGTH - self.size / 2))
+        else:
+            self.targetX = random.randrange(int(x + SQUARE_LENGTH + self.size / 2), int(SCREEN_WIDTH - self.size / 2))
+        targetY = self.size / 2
+            
+        zone = RectangleOutline(self.targetX, targetY, self.size, self.size, 0, 0, 0, 0, arcade.color.RED)
+        self.shape_list.append(zone)
+
+    def update_line(self, linVel, angVel):
+        line = self.shape_list[1]
+        line.angle = angVel
+        self.linearVelocity = linVel
+        
+    def reflect_line(self):
+        global lastChoice
+        line = self.shape_list[1]
+        while line.angle < 0:
+            line.angle = line.angle + 360
+        angle = line.angle % 360
+        if lastChoice == "Left" and (angle <= 90 or angle >= 270):
+            line.angle = (line.angle + 180)
+        if lastChoice == "Right" and (angle >= 90 and angle <= 270):
+            line.angle = (line.angle + 180)
+        #line.angle = -1 * line.angle
+
+    def simulate_ball(self):
+        self.isRunning = True
+        self.t = 0
+        self.oldX = self.shape_list[0].x
+        self.oldY = self.shape_list[0].y
+        
     def on_draw(self):
         """
         Render the screen.
         """
-
-        # This command has to happen before we start drawing
         arcade.start_render()
 
-        self.shape_list.draw()
+        for shape in self.shape_list:
+            if(self.isRunning == True):
+                angle = self.shape_list[1].angle * math.pi / 180
+                self.shape_list[0].x = self.linearVelocity * math.cos(angle) * self.t + self.shape_list[1].x
+                self.shape_list[0].y = -16*self.t*self.t + (self.linearVelocity * math.sin(angle) * self.t) + SQUARE_LENGTH
+                self.t = self.t + 0.01
+                if(self.shape_list[0].y <= 5):
+                    if(self.shape_list[0].x >= self.targetX + SQUARE_LENGTH / 2 - self.size / 2 and self.shape_list[0].x <= self.targetX - SQUARE_LENGTH / 2 + self.size / 2):
+                        self.size = int(math.floor(self.size * 0.9))
+                        self.setup()
+                    else:
+                        self.shape_list[0].x = self.oldX
+                        self.shape_list[0].y = self.oldY
+                    self.isRunning = False
+            if(self.isRunning == False or not isinstance(shape, Line)):
+                shape.draw()
 
-    def on_update(self, delta_time):
-        """ Movement and game logic """
-        self.shape_list.center_x += 1
-        self.shape_list.center_y += 1
-        self.shape_list.angle += .1
-        self.shape_list.angle -=.1
+def selectOption():
+    global angVelRButton, gameWindow, lastChoice
+    if(angVelRButton.get() != "" and angVelRButton.get() != lastChoice):
+        lastChoice = angVelRButton.get()
+        gameWindow.reflect_line()
 
+def onPlayClick():
+    global gameWindow
+    gameWindow.simulate_ball()
+
+def onChange(evt):
+    global linVelEntry, angVelEntry, gameWindow
+    if(linVelEntry.get() == '' or math.isnan(int(linVelEntry.get()))):
+        linVel = 0
+    else:
+        linVel = int(linVelEntry.get())
+    if(angVelEntry.get() == '' or math.isnan(int(angVelEntry.get()))):
+        ang = 0
+    else:
+        ang = int(angVelEntry.get())
+    gameWindow.update_line(linVel, ang)
+    
+def onExitClick():
+    global buttonWindow, gameWindow
+    buttonWindow.destroy()
+    arcade.close_window()
+
+def createWindow(app):
+    global angVelRButton, linVelEntry, angVelEntry
+    app.columnconfigure(5)
+    lengths = [2, 15, 1, 15, 2]
+    for i in range(0,len(lengths)):
+        Label(app, text=' ', width=lengths[i]).grid(row=0, column=i)
+        
+    Label(app, text='Starting Linear Velocity:').grid(row=1, column=1)
+    linVelEntry = Entry(app)
+    linVelEntry.grid(row=1, column=3)
+    linVelEntry.bind("<KeyRelease>", onChange)
+    
+    for i in range(0,len(lengths)):
+        Label(app, text=' ', width=lengths[i]).grid(row=2, column=i)
+        
+    Label(app, text='Starting Angle:').grid(row=3, column=1)
+    angVelEntry = Entry(app)
+    angVelEntry.grid(row=3, column=3)
+    angVelEntry.bind("<KeyRelease>", onChange)
+    
+    for i in range(0,len(lengths)):
+        Label(app, text=' ', width=lengths[i]).grid(row=4, column=i)
+        
+    Label(app, text='Velocity Direction:').grid(row=5, column=1)
+    Types = ["Left", "Right"]
+    angVelRButton = StringVar()
+    for i in range(0,len(Types)):
+        text = Types[i]
+        Radiobutton(app, text=text, variable=angVelRButton, value=text, command=selectOption).grid(row=6, column=2*i+1)
+         
+    for i in range(0,len(lengths)):
+        Label(app, text=' ', width=lengths[i]).grid(row=7, column=i)
+        
+    playButton = Button(app, text='Play', command=onPlayClick)
+    playButton.grid(row=8, column=1)
+    closeButton = Button(app, text='Exit', command=onExitClick)
+    closeButton.grid(row=8, column=3)
+    
+    app.geometry("320x200")
+    app.mainloop()
+
+def handleWindow():
+    global buttonWindow
+    buttonWindow = tk.Tk(className='CS 230 Final Project')
+    createWindow(buttonWindow)
 
 def main():
-    window = MyGame()
-    window.setup()
+    global gameWindow
+    buttonWindow = threading.Thread(target=handleWindow)
+    buttonWindow.start()
+    gameWindow = MyGame()
+    gameWindow.setup()
     arcade.run()
-
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,12 +1,15 @@
-
 #imports
 import arcade
 import random
 import math 
 import os 
 import timeit
-import pymunk 
+import threading
+import time
 
+from tkinter import *
+from tkinter.ttk import *
+import tkinter as tk
 
 # Set up the constants
 SCREEN_WIDTH = 800
@@ -15,32 +18,12 @@ SCREEN_GAME_Height = 300
 SCREEN_GAME_Width = 400
 SCREEN_TITLE = "CS 230 Final Project!"
 
-
-RECT_WIDTH = 50
-RECT_HEIGHT = 50
+SQUARE_LENGTH = 20
 
 NUMBER_OF_SHAPES = 1
-
-class PhysicsSprite(arcade.Sprite):
-    def __init__(self, pymunk_shape, filename):
-        super().__init__(filename, center_x=pymunk_shape.body.position.x, center_y=pymunk_shape.body.position.y)
-        self.pymunk_shape = pymunk_shape
-
-class CircleSprite(PhysicsSprite):
-    def __init__(self, pymunk_shape, filename):
-        super().__init__(pymunk_shape, filename)
-        self.width = pymunk_shape.radius * 2
-        self.height = pymunk_shape.radius * 2
-
-class BoxSprite(PhysicsSprite):
-    def __init__(self, pymunk_shape, filename, width, height):
-        super().__init__(pymunk_shape, filename)
-        self.width = width
-        self.height = height
-
+lastChoice = "Right"
 
 class Shape:
-
     def __init__(self, x, y, width, height, angle, delta_x, delta_y,
                  delta_angle, color):
         self.x = x
@@ -58,74 +41,84 @@ class Shape:
         self.y += self.delta_y
         self.angle += self.delta_angle
 
-
 class Ellipse(Shape):
-
     def draw(self):
         arcade.draw_ellipse_filled(self.x, self.y, self.width, self.height,
                                    self.color, self.angle)
 
-
 class Rectangle(Shape):
-
     def draw(self):
         arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height,
                                      self.color, self.angle)
 
-
+class RectangleOutline(Shape):
+    def draw(self):
+        arcade.draw_rectangle_outline(self.x, self.y, self.width, self.height,
+                                     self.color, 3)
+class Line(Shape):
+    def draw(self):
+        newX = 25 * math.cos(self.angle * math.pi / 180)
+        newY = 25 * math.sin(self.angle * math.pi / 180)
+        arcade.draw_line(self.x, self.y, self.x + newX, self.y + newY, self.color, 3)
+        arcade.draw_ellipse_filled(self.x + newX, self.y + newY, 10, 10, self.color, 3)
+        
 class MyGame(arcade.Window):
     """ Main application class. """
 
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-        file_path = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(os.path.dirname(__file__))
         os.chdir(file_path)
         self.shape_list = None
         arcade.set_background_color(arcade.color.BOLE)
-        
-        # Pymunk
-        self.space = pymunk.Space()
-        self.space.iterations =10
-        self.space.gravity = (0.0, -900.0)
-        
-        #Sprites---- or Lines
-        self.sprite_list: arcade.SpriteList[PhysicsSprite] = arcade.SpriteList()
-        self.static_lines = []
-
-        
+        self.size = 100
+        self.isRunning = False
 
     def setup(self):
         """ Set up the game and initialize the variables. """
         self.shape_list = []
 
-        for i in range(NUMBER_OF_SHAPES):
-            # x = random.randrange(0, SCREEN_GAME_Width)
-            # y = random.randrange(0, SCREEN_GAME_Height)
-            width = random.randrange(10, 100)
-            height = random.randrange(10, 200)
-            angle = random.randrange(0, 360)
+        x = random.randrange(SQUARE_LENGTH, SCREEN_WIDTH - SQUARE_LENGTH)
+        y = SQUARE_LENGTH / 2
 
-            # d_x = random.randrange(-3, 4)
-            # d_y = random.randrange(-3, 4)
-            # d_angle = random.randrange(-3, 4)
+        base = Rectangle(x, y, SQUARE_LENGTH, SQUARE_LENGTH, 0, 0, 0, 0, arcade.color.YELLOW)
+        self.shape_list.append(base)
+        
+        line = Line(x, y, 0, 0, 0, 0, 0, 0, arcade.color.BLACK)
+        self.shape_list.append(line)
 
-            red = random.randrange(256)
-            green = random.randrange(256)
-            blue = random.randrange(256)
-            alpha = random.randrange(256)
+        if(x > SCREEN_WIDTH - x):
+            self.targetX = random.randrange(int(self.size / 2), int(x - SQUARE_LENGTH - self.size / 2))
+        else:
+            self.targetX = random.randrange(int(x + SQUARE_LENGTH + self.size / 2), int(SCREEN_WIDTH - self.size / 2))
+        targetY = self.size / 2
+            
+        zone = RectangleOutline(self.targetX, targetY, self.size, self.size, 0, 0, 0, 0, arcade.color.RED)
+        self.shape_list.append(zone)
 
-            # shape_type = random.randrange(2)
+    def update_line(self, linVel, angVel):
+        line = self.shape_list[1]
+        line.angle = angVel
+        self.linearVelocity = linVel
+        
+    def reflect_line(self):
+        global lastChoice
+        line = self.shape_list[1]
+        while line.angle < 0:
+            line.angle = line.angle + 360
+        angle = line.angle % 360
+        if lastChoice == "Left" and (angle <= 90 or angle >= 270):
+            line.angle = (line.angle + 180)
+        if lastChoice == "Right" and (angle >= 90 and angle <= 270):
+            line.angle = (line.angle + 180)
+        #line.angle = -1 * line.angle
 
-            # shape_type == 0:
-            shape = Rectangle(70, 100, 80, 100, 270, 0, 0, 260, (208, 255, 0, 255))
-            # shape = arcade.draw_rectangle_filled(70, 260, 30, 40, arcade.color.BONE)
-            # else:
-            #     shape = Ellipse(x, y, width, height, angle, d_x, d_y,
-            #                     d_angle, (red, green, blue, alpha))
-            self.shape_list.append(shape)
-
-  
-
+    def simulate_ball(self):
+        self.isRunning = True
+        self.t = 0
+        self.oldX = self.shape_list[0].x
+        self.oldY = self.shape_list[0].y
+        
     def on_draw(self):
         """
         Render the screen.
@@ -133,14 +126,102 @@ class MyGame(arcade.Window):
         arcade.start_render()
 
         for shape in self.shape_list:
-            shape.draw()
+            if(self.isRunning == True):
+                angle = self.shape_list[1].angle * math.pi / 180
+                self.shape_list[0].x = self.linearVelocity * math.cos(angle) * self.t + self.shape_list[1].x
+                self.shape_list[0].y = -16*self.t*self.t + (self.linearVelocity * math.sin(angle) * self.t) + SQUARE_LENGTH
+                self.t = self.t + 0.01
+                if(self.shape_list[0].y <= 5):
+                    if(self.shape_list[0].x >= self.targetX + SQUARE_LENGTH / 2 - self.size / 2 and self.shape_list[0].x <= self.targetX - SQUARE_LENGTH / 2 + self.size / 2):
+                        self.size = int(math.floor(self.size * 0.9))
+                        self.setup()
+                    else:
+                        self.shape_list[0].x = self.oldX
+                        self.shape_list[0].y = self.oldY
+                    self.isRunning = False
+            if(self.isRunning == False or not isinstance(shape, Line)):
+                shape.draw()
 
+def selectOption():
+    global angVelRButton, gameWindow, lastChoice
+    if(angVelRButton.get() != "" and angVelRButton.get() != lastChoice):
+        lastChoice = angVelRButton.get()
+        gameWindow.reflect_line()
+
+def onPlayClick():
+    global gameWindow
+    gameWindow.simulate_ball()
+
+def onChange(evt):
+    global linVelEntry, angVelEntry, gameWindow
+    if(linVelEntry.get() == '' or math.isnan(int(linVelEntry.get()))):
+        linVel = 0
+    else:
+        linVel = int(linVelEntry.get())
+    if(angVelEntry.get() == '' or math.isnan(int(angVelEntry.get()))):
+        ang = 0
+    else:
+        ang = int(angVelEntry.get())
+    gameWindow.update_line(linVel, ang)
+    
+def onExitClick():
+    global buttonWindow, gameWindow
+    buttonWindow.destroy()
+    arcade.close_window()
+
+def createWindow(app):
+    global angVelRButton, linVelEntry, angVelEntry
+    app.columnconfigure(5)
+    lengths = [2, 15, 1, 15, 2]
+    for i in range(0,len(lengths)):
+        Label(app, text=' ', width=lengths[i]).grid(row=0, column=i)
+        
+    Label(app, text='Starting Linear Velocity:').grid(row=1, column=1)
+    linVelEntry = Entry(app)
+    linVelEntry.grid(row=1, column=3)
+    linVelEntry.bind("<KeyRelease>", onChange)
+    
+    for i in range(0,len(lengths)):
+        Label(app, text=' ', width=lengths[i]).grid(row=2, column=i)
+        
+    Label(app, text='Starting Angle:').grid(row=3, column=1)
+    angVelEntry = Entry(app)
+    angVelEntry.grid(row=3, column=3)
+    angVelEntry.bind("<KeyRelease>", onChange)
+    
+    for i in range(0,len(lengths)):
+        Label(app, text=' ', width=lengths[i]).grid(row=4, column=i)
+        
+    Label(app, text='Velocity Direction:').grid(row=5, column=1)
+    Types = ["Left", "Right"]
+    angVelRButton = StringVar()
+    for i in range(0,len(Types)):
+        text = Types[i]
+        Radiobutton(app, text=text, variable=angVelRButton, value=text, command=selectOption).grid(row=6, column=2*i+1)
+         
+    for i in range(0,len(lengths)):
+        Label(app, text=' ', width=lengths[i]).grid(row=7, column=i)
+        
+    playButton = Button(app, text='Play', command=onPlayClick)
+    playButton.grid(row=8, column=1)
+    closeButton = Button(app, text='Exit', command=onExitClick)
+    closeButton.grid(row=8, column=3)
+    
+    app.geometry("320x200")
+    app.mainloop()
+
+def handleWindow():
+    global buttonWindow
+    buttonWindow = tk.Tk(className='CS 230 Final Project')
+    createWindow(buttonWindow)
 
 def main():
-    window = MyGame()
-    window.setup()
+    global gameWindow
+    buttonWindow = threading.Thread(target=handleWindow)
+    buttonWindow.start()
+    gameWindow = MyGame()
+    gameWindow.setup()
     arcade.run()
-
 
 if __name__ == "__main__":
     main()
